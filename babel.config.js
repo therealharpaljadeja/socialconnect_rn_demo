c9ce77846942c8f8e0259de3bcb98d6a66867740
Harpal Jadeja <jadejaharpal14@gmail.com>
2023-02-02 22:21:55 +0530

    feat: add `react-native-dotenv`

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['module:react-native-dotenv', {path: '.env'}],
+  plugins: [['module:react-native-dotenv', {path: '.env'}]],
 };
